7acb0053ff09481789b684521497750d364d6a3c
fatimasarrsaliou-stack <fatimasarrsaliou@gmail.com>
2025-11-25 21:56:18 +0100

    Add files via upload

---
+++ tailwind.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        night: "#0A0F2F",
+        gold: "#D4AF37",
+      },
+    },
+  },
+  plugins: [],
+};
